c1141f61267dfcd4bc9817a96ff3fd26d4502516
Eryk Mroczko <e.mroczko@stud.elka.pw.edu.pl>
2020-04-05 12:43:21 +0200

    7

--- main.py
+++ main.py
@@ -8,16 +8,28 @@ app = FastAPI()
 def root():
     return {"message": "Hello World during the coronavirus pandemic!"}
 '''
-# main.py
-
 from fastapi import FastAPI
+
+from pydantic import BaseModel
 
 app = FastAPI()
 
+
+
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"method": "GET"}
 
-@app.get("/hello/{name}")
-async def read_item(name: str):
-    return f"Hello {name}"
+@app.post("/")
+def root():
+    return {"method": "POST"}
+
+@app.put("/")
+def root():
+    return {"method": "PUT"}
+
+@app.delete("/")
+def root():
+    return {"method": "DELETE"}
+
+
