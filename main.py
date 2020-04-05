943c51fbb07567b5898896aeaad3edfe03c1d4af
Eryk Mroczko <e.mroczko@stud.elka.pw.edu.pl>
2020-04-05 12:30:51 +0200

    6

--- main.py
+++ main.py
@@ -8,29 +8,16 @@ app = FastAPI()
 def root():
     return {"message": "Hello World during the coronavirus pandemic!"}
 '''
+# main.py
+
 from fastapi import FastAPI
 
-from pydantic import BaseModel
-
 app = FastAPI()
-# to see what funny will come
-app.counter = 0
-
-
-class HelloResp(BaseModel):
-    msg: str
 
 @app.get("/")
 def root():
-    return {"method": "Hello World"}
+    return {"message": "Hello World"}
 
-
-@app.get('/counter')
-def counter():
-    app.counter += 1
-    return str(app.counter)
-
-
-@app.get("/", response_model=HelloResp)
+@app.get("/hello/{name}")
 async def read_item(name: str):
-    return HelloResp(msg=f"Hello {name}")
+    return f"Hello {name}"
